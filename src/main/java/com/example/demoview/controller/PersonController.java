2a09cb17c6e812e3b8d61545d096d12231c7edaf
nicolanaz <nicola.dzr@gmail.com>
2022-10-18 21:46:46 +0300

    rsocket added

--- src/main/java/com/example/demoview/controller/PersonController.java
+++ src/main/java/com/example/demoview/controller/PersonController.java
@@ -1,22 +1,36 @@
 package com.example.demoview.controller;
 
+import com.example.demoview.model.Person;
 import com.example.demoview.model.PersonRepository;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Flux;
 
 @Controller
-@RequiredArgsConstructor
 public class PersonController {
-    @NonNull
     private PersonRepository repository;
+    private RSocketRequester requester;
+
+    public PersonController(PersonRepository repository, RSocketRequester.Builder builder) {
+        this.repository = repository;
+        this.requester = builder.tcp("localhost", 7634);
+    }
 
     @GetMapping("/people")
     public String getPeople(Model model) {
         model.addAttribute("listOfPeople", repository.findAll());
         return "people";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/peopleStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Person> getPeopleStream() {
+        return requester.route("peopleStream")
+                .data("Retrieve People")
+                .retrieveFlux(Person.class);
     }
 }
